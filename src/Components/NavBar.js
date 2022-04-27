a787e1df61b1be0355487012751d194574d631fa
Benjamin Kleeman <benjamin.kleeman@gmail.com>
2022-04-27 16:55:59 -0500

    improve navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,21 +1,27 @@
 import { Link } from "react-router-dom";
+import Typography from '@material-ui/core/Typography';
+import { Container, Paper } from "@material-ui/core";
 
 export default function NavBar() {
     return(
-    <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-          marginBottom: "1rem"
-        }}
-      >
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/introduction">Introduction</Link> |{" "}
-        <Link to="/code-examples">Code Examples</Link> |{" "}
-        <Link to="/endpoints">Endpoints</Link> |{" "}
-        <Link to="/schema">Schema</Link> |{" "}
-        <Link to="/coverage">Coverage</Link> |{" "}
-        <Link to="/us-energy-infrastructure-visualization">US Energy Infrastructure Visualization</Link>
-      </nav>
+      <Container>
+        <nav
+            style={{
+              borderBottom: "solid 1px",
+              paddingBottom: "1rem",
+              marginBottom: "1rem",
+              textAlign: 'center'
+            }}
+          >
+          <Link to="/"><Typography variant="body1" component="body1" gutterBottom>Home</Typography></Link> |{" "}
+          <Link to="/introduction"><Typography variant="body1" component="body1" gutterBottom>Introduction</Typography></Link> |{" "}
+          <Link to="/code-examples"><Typography variant="body1" component="body1" gutterBottom>Code Examples</Typography></Link> |{" "}
+          <Link to="/endpoints"><Typography variant="body1" component="body1" gutterBottom>Endpoints</Typography></Link> |{" "}
+          <Link to="/schema"><Typography variant="body1" component="body1" gutterBottom>Schema</Typography></Link> |{" "}
+          <Link to="/coverage"><Typography variant="body1" component="body1" gutterBottom>Coverage</Typography></Link> |{" "}
+          <Link to="/us-energy-infrastructure-visualization"><Typography variant="body1" component="body1" gutterBottom>US Energy Infrastructure Visualization</Typography></Link>
+        </nav>
+      </Container>  
+      
     )
 }
