67536a387c86bcb5bc79a9b512f9535ed82a2c3b
Benjamin Kleeman <benjamin.kleeman@gmail.com>
2022-04-27 20:15:14 -0500

    fix formatting on energy viz page

--- src/routes/us-energy-infrastructure-visualization.js
+++ src/routes/us-energy-infrastructure-visualization.js
@@ -18,18 +18,21 @@ export default function UsEnergyInfrastructureVisualization() {
                         <br/>
                         <Container>
                             <Paper elevation={12}>
-                                <Typography variant='h5' component='h2'>
-                                    US Energy Infrastructure Visualization
-                                </Typography>
-                                <Typography variant='body1' component='body1'>
-                                    The US Energy Infrastructure Visualization is a data visualization created by Nathan Matteson and Benjamin Kleeman through the <a href='https://rdcep.org' target='_blank' rel='noreferrer'>Center for Research-based Decision Making on Climate and Energy Policy</a> (RDCEP) that documents all long-lived infrastructure (> 20 years service life) associated with extracting, processing, transporting, and converting energy in the U.S.
-                                    <br/><br/>
-                                    The project is an effort of the University of Chicago’s Center for Robust Decision-making on Climate and Energy Policy (RDCEP). Its research is documented in Chen et al 2020, “A Physical Inventory of the U.S. Energy System”, which includes documentation on data sources, validation, and comparison to estimates of depreciated value (~50% of replacement value). This paper is currently in preparation for peer review and is available here as a preprint. See also [www.rdcep.org]) for further information. All data are available on request.
-                                </Typography>
-                                <br/>
-                                <Container maxWidth="m"  className="App">
-                                    <img src={viz} className="request-image" alt="A map of the US showing railroads and coal plants."></img>
-                                </Container>
+                                <div className="main-paper-content">
+                                    <Typography variant='h5' component='h2'>
+                                        US Energy Infrastructure Visualization
+                                    </Typography>
+                                    <br/>
+                                    <Typography variant='body1' component='body1'>
+                                        The US Energy Infrastructure Visualization is a data visualization created by Nathan Matteson and Benjamin Kleeman through the <a href='https://rdcep.org' target='_blank' rel='noreferrer'>Center for Research-based Decision Making on Climate and Energy Policy</a> (RDCEP) that documents all long-lived infrastructure (> 20 years service life) associated with extracting, processing, transporting, and converting energy in the U.S.
+                                        <br/><br/>
+                                        The project is an effort of the University of Chicago’s Center for Robust Decision-making on Climate and Energy Policy (RDCEP). Its research is documented in Chen et al 2020, “A Physical Inventory of the U.S. Energy System”, which includes documentation on data sources, validation, and comparison to estimates of depreciated value (~50% of replacement value). This paper is currently in preparation for peer review and is available here as a preprint. See also [www.rdcep.org]) for further information. All data are available on request.
+                                    </Typography>
+                                    <br/>
+                                    <Container maxWidth="m"  className="App">
+                                        <img src={viz} className="request-image" alt="A map of the US showing railroads and coal plants."></img>
+                                    </Container>
+                                </div>
                             </Paper>
                     </Container>
                 </Paper>
